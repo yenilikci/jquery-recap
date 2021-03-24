e6909f93c0cbc52cff343e987fd4212714871449
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-03-24 22:42:41 +0300

    Mouse&Keyboard Events

---
+++ 04-mousekeyboardevents.js
@@ -0,0 +1,79 @@
+//JQuery Mouse&Keyboard Events
+
+$(function () {
+
+   /* tıklama
+    $("#btn").click(function (){
+        $("#div1").css("background-color","red");
+    });
+    */
+
+   /* çift tıklama
+   $("#btn").dblclick(function (){
+       $("#div1").css("background-color","red");
+   });
+   */
+
+   /* mouse ile elementin üzerine gelme
+   $("#div1").mouseenter(function (){
+      $("#div1").css("background-color","red");
+   });
+   */
+
+   /* mouse ile elementin terk etme
+   $("#div1").mouseleave(function (){
+      $("#div1").css("background-color","red");
+   })
+   */
+
+   /* üzerinde
+   $("#div1").mouseover(function (){
+      $("#div1").css("background-color","red");
+   });
+   */
+
+   /* çıkınca
+   $("#div1").mouseout(function (){
+      $("#div1").css("background-color","blue")
+   });
+   */
+
+   /* css teki hover
+   $("#div1").hover(function (){
+      $("#div1").css("background-color","red")
+   });
+   */
+
+   /* mouse sol tık basılı değilken
+   $("#div1").mouseup(function (){
+      $("#div1").css("background-color","red");
+   });
+   */
+
+   /* mouse sol tık basılı iken
+   $("#div1").mousedown(function (){
+       $("#div1").css("background-color","blue");
+   })
+   */
+
+   /* inputun içine yazı yazılırsa
+   $("#txt").keypress(function (){
+      $("#div1").css("background-color","red");
+   });
+   */
+
+   /* tuşa bastıktan sonra elimizi kaldırmamız lazım
+   $("#txt").keyup(function (){
+       $("#div1").css("background-color","red");
+   });
+   */
+
+
+   /* tuşa bastıktan sonra direkt aktif olur
+   $("#txt").keydown(function (){
+       $("#div1").css("background-color","red");
+   });
+   */
+
+
+})
